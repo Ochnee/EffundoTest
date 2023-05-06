e091beb8927f2fbe86326559c8a9ca972ac42fb6
Andreas <andreas.bosteen@gmail.com>
2023-05-06 15:44:32 +0200

    Cookie consent

--- EffundoTest/EffundoTest/Program.cs
+++ EffundoTest/EffundoTest/Program.cs
@@ -1,3 +1,4 @@
+using BytexDigital.Blazor.Components.CookieConsent;
 using EffundoTest;
 using EffundoTest.Services;
 using Microsoft.AspNetCore.Components.Web;
@@ -8,7 +9,54 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddSingleton<MockPageService>(x => new MockPageService(10));
+builder.Services.AddSingleton(x => new MockPageService(10));
+
+builder.Services.AddCookieConsent(o =>
+{
+    o.Revision = 1;
+    o.PolicyUrl = "/cookie-policy";
+
+    // Call optional
+    o.UseDefaultConsentPrompt(prompt =>
+    {
+        prompt.Position = ConsentModalPosition.BottomRight;
+        prompt.Layout = ConsentModalLayout.Bar;
+        prompt.SecondaryActionOpensSettings = true;
+        prompt.AcceptAllButtonDisplaysFirst = false;
+    });
+
+    o.Categories.Add(new CookieCategory
+    {
+        TitleText = new()
+        {
+            ["en"] = "PayPal Services"
+        },
+        DescriptionText = new()
+        {
+            ["en"] = "Allows for the safe usage of the PayPal donation buttons on this website."
+        },
+        Identifier = "PayPal",
+        IsPreselected = true,
+
+        Services = new()
+        {
+            new CookieCategoryService
+            {
+                Identifier = "PayPal donation button",
+                PolicyUrl = "https://policies.google.com/privacy",
+                TitleText = new()
+                {
+                    ["en"] = "PayPal Donation Button"
+                },
+                ShowPolicyText = new()
+                {
+                    ["en"] = "Display policies",
+                    ["de"] = "Richtlinien anzeigen"
+                }
+            }
+        }
+    });
+});
 
 await builder.Build().RunAsync();
 
---
+++ EffundoTest/EffundoTest/Shared/Footer.razor.cs
@@ -0,0 +1,33 @@
+﻿using BytexDigital.Blazor.Components.CookieConsent;
+using Microsoft.AspNetCore.Components;
+
+namespace EffundoTest.Shared
+{
+    public partial class Footer : IDisposable
+    {
+        [Inject]protected CookieConsentService cookieConsentService { get; set; }
+        protected bool DisplayCookieSettingsButton { get; set; } = false;
+        protected override async Task OnParametersSetAsync()
+        {
+            cookieConsentService.CookiePreferencesChanged += UpdateDisplayCookieButton;
+            await UpdateDisplayCookieButton();
+        }
+
+        public async Task UpdateDisplayCookieButton()
+        {
+            DisplayCookieSettingsButton = (await cookieConsentService.GetPreferencesAsync())
+                                          .AcceptedRevision == 1;
+            StateHasChanged();
+        }
+        public void UpdateDisplayCookieButton(object sender, CookiePreferences preferences)
+        {
+            DisplayCookieSettingsButton = preferences.AcceptedRevision == 1;
+            StateHasChanged();
+        }
+
+        public void Dispose()
+        {
+            cookieConsentService.CookiePreferencesChanged -= UpdateDisplayCookieButton;
+        }
+    }
+}
